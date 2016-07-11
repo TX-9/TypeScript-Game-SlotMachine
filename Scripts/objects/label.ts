fbfc338edad91d7b6bf06726b4db8ce33d10f0fa
TX-9 <jacobleecs@gmail.com>
2016-07-11 09:59:04 -0400

    last modified

--- Scripts/objects/label.ts
+++ Scripts/objects/label.ts
@@ -1,4 +1,13 @@
-﻿module objects {
+﻿/* 
+Author: Changbae Lee(300770812)
+Service URL: http://changbaelee-slotmachine.azurewebsites.net/
+Description: Web App called Slot machine is developed with CreateJS framework
+Revision History: It is managed with GitHub (https://github.com/TX-9/COMP397-S2016-SlotMachine)
+Last Modified by: Changbae Lee
+Last Modified date: Jul 11, 2016
+*/
+
+module objects {
     // LABEL CLASS ++++++++++++++++++++++++++++++++++++++++++++++
     export class Label extends createjs.Text {
         // CONSTRUCTOR METHOD +++++++++++++++++++++++++++++++++++
